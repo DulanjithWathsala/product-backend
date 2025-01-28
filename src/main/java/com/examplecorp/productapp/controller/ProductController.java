9d05780f27c164e81a4bbcf31254d25e922584ea
M.D.W.De Silva <wdulanjith@gmail.com>
2025-01-28 15:13:23 +0530

    param type changed

--- src/main/java/com/examplecorp/productapp/controller/ProductController.java
+++ src/main/java/com/examplecorp/productapp/controller/ProductController.java
@@ -4,10 +4,7 @@ import com.examplecorp.productapp.dto.ProductDto;
 import com.examplecorp.productapp.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +15,9 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/all-by/page/{pageNumber}/size/{pageSize}")
-    public ResponseEntity<List<ProductDto>> retrieveAll(@PathVariable Integer pageNumber,
-                                                        @PathVariable Integer pageSize) {
+    @GetMapping("/all-by")
+    public ResponseEntity<List<ProductDto>> retrieveAll(@RequestParam(defaultValue = "0") Integer pageNumber,
+                                                        @RequestParam(defaultValue = "10") Integer pageSize) {
         return productService.retrieveAll(pageNumber, pageSize);
     }
 
